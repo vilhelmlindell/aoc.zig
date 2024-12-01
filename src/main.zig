414be93a25cedc5166d553a60dfe812479a284e1
Vilhelm Lindell <95926200+vilhelmlindell@users.noreply.github.com>
2024-12-01 18:29:26 +0100

    Initial commit

---
+++ src/main.zig
@@ -0,0 +1,31 @@
+const std = @import("std");
+const fs = std.fs;
+const io = std.io;
+const heap = std.heap;
+
+const Problem = @import("problem");
+
+pub fn main() !void {
+    const stdout = io.getStdOut().writer();
+
+    var arena = heap.ArenaAllocator.init(std.heap.page_allocator);
+    defer arena.deinit();
+    const allocator = arena.allocator();
+
+    const problem = Problem{
+        .input = @embedFile("input"),
+        .allocator = allocator,
+    };
+
+    if (try problem.part1()) |solution|
+        try stdout.print(switch (@TypeOf(solution)) {
+            []const u8 => "{s}",
+            else => "{any}",
+        } ++ "\n", .{solution});
+
+    if (try problem.part2()) |solution|
+        try stdout.print(switch (@TypeOf(solution)) {
+            []const u8 => "{s}",
+            else => "{any}",
+        } ++ "\n", .{solution});
+}
