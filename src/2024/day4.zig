267b12c66c5e7d2c0e2a43277f3cc0a6949b8d58
Vilhelm Lindell <vilhelm.lindell@gmail.com>
2024-12-07 11:53:24 +0100

    stuff

--- src/2024/day4.zig
+++ src/2024/day4.zig
@@ -6,15 +6,28 @@ allocator: mem.Allocator,
 
 pub fn part1(this: *const @This()) !?i64 {
     var lines = std.mem.splitScalar(u8, this.input, '\n');
-    const width = lines.peek().?.len;
-    const directions = [8]i32{1, -1, width, -width, width + 1, width - 1, -width + 1, -width - 1};
-    var wordSearch = std.ArrayList([width]u8).init(this.allocator);
+    var wordSearch = std.ArrayList([]const u8).init(this.allocator);
 
     while (lines.next()) |line| {
-        wordSearch.append(line);
+        try wordSearch.append(line);
     }
 
+    const width: i32 = @intCast(wordSearch.items[0].len);
+    const height = wordSearch.items.len;
 
+    const directions = [8]i32{ 1, -1, width, -width, width + 1, width - 1, -width + 1, -width - 1 };
+
+    for (0..height) |y| {
+        for (0..@intCast(width)) |x| {
+            if (wordSearch.items[y][x] == 'X') {
+                for (directions) |direction| {
+                    y += @divFloor(direction, width);
+                    x += direction % width;
+                }
+                std.debug.print("x: {d} y: {d}\n", .{ x, y });
+            }
+        }
+    }
 
     return null;
 }
